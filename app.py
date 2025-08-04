ad28070db393431acf99ba0389062a895ac44f21
unknown <abhishekshukla2711@gmail.com>
2025-08-04 19:47:04 +0530

    Add session management

--- app.py
+++ app.py
@@ -5,7 +5,57 @@ from resume_paser import parse_resume
 import fitz  # PyMuPDF
 from dotenv import load_dotenv
 from streamlit_extras.badges import badge
+import firebase_admin 
+import requests
+from firebase_admin import credentials ,auth as admin_auth
+import urllib.parse
+from urllib.parse import urlparse,parse_qs
+from firebase_config import auth
 import os
+
+#----Session Management-----#
+query_params = st.query_params
+if "token" in query_params:
+    token=query_params["token"][0]
+    st.session_state["token"]=token
+
+is_logged_in="token" in st.session_state
+
+if not is_logged_in:
+    st.warning("Please login in to use Resume Ranker.")
+    st.stop()
+
+st.sidebar.success("✅ Logged in")
+if st.sidebar.button("Logout"):
+    st.session_state.clear()
+    st.experimental_rerun()
+
+
+if not firebase_admin._apps:
+    cred=credentials.Certificate("resume-ranker-auth-firebase-adminsdk-fbsvc-16ac3f1d73")
+    firebase_admin.initialize_app(cred)
+
+    def verify_token(id_token):
+        try:
+            decoded_token=admin_auth.verify_id_token(id_token)
+            return decoded_token
+        except Exception as e:
+            st.error("Invalid or expired token.")
+            return None
+    
+query_params=st.experimental_get_query_params()
+if "token" in query_params:
+    id_token=query_params["token"][0]
+    user_info=verify_token(id_token)
+    if user_info:
+        st.session_state["user"]=user_info
+        st.success(f"Welcome {user_info['email']}!")
+    else:
+        st.stop()
+    
+elif "user" not in st.session_state:
+    st.markdown("[Click here to login with Google]")
+    st.stop()
 
 load_dotenv()
 

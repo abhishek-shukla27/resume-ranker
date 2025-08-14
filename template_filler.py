993130e6f49f26b84be55d258ee6a2ea623d18ec
unknown <abhishekshukla2711@gmail.com>
2025-08-14 21:03:45 +0530

    issue fixed

--- template_filler.py
+++ template_filler.py
@@ -80,7 +80,7 @@ def add_heading_with_line(doc, text):
     para = doc.add_paragraph(text.upper())
     run = para.runs[0]
     run.bold = True
-    run.font.size = Pt(12)
+    run.font.size = Pt(6)
 
     # Horizontal line
     p = doc.add_paragraph()
@@ -89,8 +89,8 @@ def add_heading_with_line(doc, text):
     p_borders = OxmlElement("w:pBdr")
     bottom = OxmlElement("w:bottom")
     bottom.set(qn("w:val"), "single")
-    bottom.set(qn("w:sz"), "12")
-    bottom.set(qn("w:space"), "1")
+    bottom.set(qn("w:sz"), "6")
+    bottom.set(qn("w:space"), "0")
     bottom.set(qn("w:color"), "000000")
     p_borders.append(bottom)
     p_pr.append(p_borders)
